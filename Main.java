e247bb7f283496559b8f7b46adb21229091af51d
mam10023 <mam10023@nyu.edu>
2024-08-11 11:33:59 -0400

    Created method to clear previous emails at startup

--- Main.java
+++ Main.java
@@ -1,7 +1,25 @@
 public class main {
 	public static void main(String[] args) {
-        //initialize and show login frame upon running program
+
+		// not sure if we want this -> deletes emails at startup **
+		File emailDir = new File("emails");
+		deleteDirectory(emailDir);
+
+		// initialize and show login frame upon running program
 		LoginGUI loginFrame1 = new LoginGUI();
 		loginFrame1.setVisible(true);
+	}
+
+	// **
+	private static void deleteDirectory(File file) {
+		if (file.isDirectory()) {
+			File[] files = file.listFiles();
+			if (files != null) {
+				for (File f : files) {
+					deleteDirectory(f);
+				}
+			}
+		}
+		file.delete();
 	}
 }
