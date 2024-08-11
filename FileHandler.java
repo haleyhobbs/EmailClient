5a028dcb27f1a88caf97c1221f1dce03b628eed7
mam10023 <mam10023@nyu.edu>
2024-08-11 11:24:25 -0400

    create filehandler and email directory string

--- FileHandler.java
+++ FileHandler.java
@@ -1,1 +1,10 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class FileHandler {
+
+    private static final String EMAIL_DIR = "emails";
+
+}
