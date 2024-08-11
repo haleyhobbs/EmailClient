afe471530ef33af9aaef31c308f332424a2c71a0
mam10023 <mam10023@nyu.edu>
2024-08-11 11:27:55 -0400

    Created read email method

--- FileHandler.java
+++ FileHandler.java
@@ -37,6 +37,33 @@ public class FileHandler {
                 writer.write(email.getEmailContent() + "\n");
             }
         }
+
+    }
+
+    public List<Email> readEmail(String userEmail) throws IOException {
+        List<Email> emails = new ArrayList<>();
+        Path userDir = Paths.get(EMAIL_DIR, userEmail);
+        if (Files.exists(userDir)) {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(userDir)) {
+                for (Path entry : stream) {
+                    if (Files.isRegularFile(entry)) {
+
+                        try (BufferedReader reader = Files.newBufferedReader(entry)) {
+                            String sender = reader.readLine().split(": ")[1];
+                            String recipient = reader.readLine().split(": ")[1];
+                            String subject = reader.readLine().split(": ")[1];
+                            StringBuilder emailContent = new StringBuilder();
+                            String line;
+                            while ((line = reader.readLine()) != null) {
+                                emailContent.append(line).append("\n");
+                            }
+                            emails.add(new Email(sender, recipient, subject, emailContent.toString()));
+                        }
+                    }
+                }
+            }
+        }
+        return emails;
     }
 
 }
