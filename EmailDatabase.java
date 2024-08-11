629fd08d509851acb3c062a214a5c3ed8f06f233
Haley Hobbs <106222701+haleyhobbs@users.noreply.github.com>
2024-08-11 20:14:38 +0200

    minor polishing

--- EmailDatabase.java
+++ EmailDatabase.java
@@ -1,9 +1,10 @@
+//libraries
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailDatabase {
-
+    //fields
     private List<Email> emails;
     private FileHandler fileHandler;
     private String loggedInUser;
@@ -18,7 +19,6 @@ public class EmailDatabase {
         loggedInUser = userEmail;
         try {
             emails = fileHandler.readEmail(userEmail);
-
         } catch (IOException e) {
             emails = new ArrayList<>();
             e.printStackTrace();
@@ -26,42 +26,33 @@ public class EmailDatabase {
     }
 
     public void saveUserEmails() throws IOException {
-        if (loggedInUser == null) {
+        if (loggedInUser == null)
             throw new IllegalStateException("No user is logged in.");
-        }
         try {
             fileHandler.saveEmail(loggedInUser, emails);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void sendEmail(Email email) throws IOException {
-
         EmailDatabase recipientEmailDB = new EmailDatabase(email.getRecipient());
         recipientEmailDB.loadUserEmails(email.getRecipient());
         recipientEmailDB.emails.add(email);
-
         recipientEmailDB.saveUserEmails();
-
     }
 
     public List<Email> getInbox(String userEmail) throws IOException {
         List<Email> inbox = new ArrayList<>();
         for (Email e : emails) {
-            if (e.getRecipient().equals(userEmail)) {
+            if (e.getRecipient().equals(userEmail))
                 inbox.add(e);
-            }
         }
-
         return inbox;
     }
 
     public void deleteEmail(String userEmail, Email emailDel, EmailDatabase emailDataBase) throws IOException {
-
         emails.remove(emailDel);
-
         saveUserEmails();
     }
 }
