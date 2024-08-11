338fc63b6cf812ef2b80a501ab8f7be07a50bf16
Haley Hobbs <106222701+haleyhobbs@users.noreply.github.com>
2024-08-11 22:29:01 +0200

    attributes

--- EmailViewerGUI.java
+++ EmailViewerGUI.java
@@ -3,6 +3,10 @@ import javax.swing.border.EmptyBorder;
 
 public class EmailViewerGUI extends JFrame {
     private JPanel contentPane;
+    private JTextArea textArea;
+    private JScrollPane scrollPane;
+    private JButton btnReply;
+    private JButton btnClose;
 
     public EmailViewerGUI(Email email, FileHandler fileHandler, String userEmail) {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -13,18 +17,18 @@ public class EmailViewerGUI extends JFrame {
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        JTextArea textArea = new JTextArea();
+        textArea = new JTextArea();
         textArea.setEditable(false);
         textArea.setText("From: " + email.getSender() + "\n"
                 + "To: " + email.getRecipient() + "\n"
                 + "Subject: " + email.getSubject() + "\n"
                 + "Message: " + email.getEmailContent());
 
-        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane = new JScrollPane(textArea);
         scrollPane.setBounds(5, 5, 440, 233);
         contentPane.add(scrollPane);
 
-        JButton btnReply = new JButton("Reply");
+        btnReply = new JButton("Reply");
         btnReply.setBounds(10, 240, 100, 30);
         btnReply.addActionListener(e -> {
             //automatically open the new ComposeGUI with the sender's email as the recipient
@@ -34,7 +38,7 @@ public class EmailViewerGUI extends JFrame {
         });
         contentPane.add(btnReply);
 
-        JButton btnClose = new JButton("Close");
+        btnClose = new JButton("Close");
         btnClose.setBounds(290, 237, 100, 30);
         btnClose.addActionListener(e -> dispose());
         contentPane.add(btnClose);
