efebd8cf08d351cd25fc66a41cb2eb89216688ba
Haley Hobbs <106222701+haleyhobbs@users.noreply.github.com>
2024-08-11 20:05:06 +0200

    minor polishing

--- Main.java
+++ Main.java
@@ -1,25 +1,24 @@
+//libraries
 import java.io.File;
 
 public class Main {
 	public static void main(String[] args) {
-
-		// not sure if we want this -> deletes emails at startup **
+		//delete emails at startup
 		File emailDir = new File("emails");
 		deleteDirectory(emailDir);
 
-		// initialize and show login frame upon running program
+		//initialize and show login frame upon running program
 		LoginGUI loginFrame1 = new LoginGUI();
 		loginFrame1.setVisible(true);
 	}
 
-	// **
+	//delete emails at startup
 	private static void deleteDirectory(File file) {
 		if (file.isDirectory()) {
 			File[] files = file.listFiles();
 			if (files != null) {
-				for (File f : files) {
+				for (File f : files)
 					deleteDirectory(f);
-				}
 			}
 		}
 		file.delete();
