2a2c06ea6530bff6ea77e9fcfac9e7c185f54cf2
mam10023 <mam10023@nyu.edu>
2024-08-11 11:13:13 -0400

    create EmailDataBase class and constructor

--- EmailDatabase.java
+++ EmailDatabase.java
@@ -1,1 +1,16 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class EmailDatabase {
+
+    private List<Email> emails;
+    private FileHandler fileHandler;
+    private String loggedInUser;
+
+    public EmailDatabase(String sender) {
+        fileHandler = new FileHandler();
+        emails = new ArrayList<>();
+        loggedInUser = sender;
+    }
+}
