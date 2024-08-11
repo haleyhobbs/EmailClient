7877fe1bad1ba67aeb868d956f0247c636ba027b
Haley Hobbs <106222701+haleyhobbs@users.noreply.github.com>
2024-08-11 20:15:23 +0200

    minor polishing

--- FileHandler.java
+++ FileHandler.java
@@ -1,11 +1,9 @@
-//libraries
 import java.io.*;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileHandler {
-    //fields
     private static final String EMAIL_DIR = "emails";
 
     public FileHandler() {
